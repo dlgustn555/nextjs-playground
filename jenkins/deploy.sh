126f78bbcd4628e2a03cee35aade3ef8234cff28
hyunsu-lee <hyunsu.lee@navercorp.com>
2023-07-21 11:16:14 +0900

    feat: 젠킨스설치 (#10)

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env sh
 
+git pull
+
 mkdir ~/jenkins
 cd ~/jenkins
 
-docker run --name jenkins -d -p 8080:8080 -v ~/jenkins:/var/jenkins_home -u root jenkins/jenkins:latest
+# docker run --name jenkins -d -p 8080:8080 -v ~/jenkins:/var/jenkins_home -u root jenkins/jenkins:latest
+
+dcoerk-compose down
+
+docker-compose up -d
+
+docker rmi $(docker images -f "dangling=true" -q)
