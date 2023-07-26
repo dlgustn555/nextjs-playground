43a8248bf0410815d2d2b0effbde87c512269667
hyunsu-lee <hyunsu.lee@navercorp.com>
2023-07-26 17:20:09 +0900

    feat:kubenetes 학습(#14)

--- src/fastcampus/kubenetes/Part3/ch03/deploy.sh
+++ src/fastcampus/kubenetes/Part3/ch03/deploy.sh
@@ -4,13 +4,25 @@ echo 'blue 디렉토리로 이동'
 cd blue
 
 
-echo '========================='
+echo '========== blue ============='
 
 echo 'blue Dockerfile 빌드'
 echo 'build -t dlgustn555/blue .'
 docker build -t dlgustn555/blue .
 
-echo '========================='
+echo 'bule Docker Image PUSH!!'
+echo 'docker push dlgustn555/blue'
+docker push dlgustn555/blue
+
+echo 'kubenetes Pod blue-pod 삭제'
+echo 'kubectl delete pod blue-pod'
+kubectl delete pod blue-pod
+
+echo 'kubenetes Pod blue-pod 실행'
+echo 'kubectl apply -f blue.yaml'
+kubectl apply -f blue.yaml
+
+echo '============ green ============'
 
 echo 'green 디렉토리로 이동'
 cd ../green
@@ -19,7 +31,19 @@ echo 'green Dockerfile 빌드'
 echo 'build -t dlgustn555/green .'
 docker build -t dlgustn555/green .
 
-echo '========================='
+echo 'docker push dlgustn555/green'
+docker push dlgustn555/green
+
+
+echo 'kubenetes Pod green-pod 삭제'
+echo 'kubectl delete pod green-pod'
+kubectl delete pod green-pod
+
+echo 'kubenetes Pod green-pod 실행'
+echo 'kubectl apply -f green.yaml'
+kubectl apply -f green.yaml
+
+echo '============ red ============='
 
 echo 'red 디렉토리로 이동'
 cd ../red
@@ -28,51 +52,12 @@ echo 'red Dockerfile 빌드'
 echo 'build -t dlgustn555/red .'
 docker build -t dlgustn555/red .
 
-echo '========================='
-
-echo 'Docker Image PUSH!!'
-
-echo 'docker push dlgustn555/blue'
-docker push dlgustn555/blue
-
-echo 'docker push dlgustn555/green'
-docker push dlgustn555/green
-
 echo 'docker push dlgustn555/red'
 docker push dlgustn555/red
-
-
-echo '========================='
-
-echo 'kubenetes Pod blue-pod 삭제'
-echo 'kubectl delete pod blue-pod'
-kubectl delete pod blue-pod
-
-echo '========================='
-
-echo 'kubenetes Pod green-pod 삭제'
-echo 'kubectl delete pod green-pod'
-kubectl delete pod green-pod
-
-echo '========================='
 
 echo 'kubenetes Pod red-pod 삭제'
 echo 'kubectl delete pod red-pod'
 kubectl delete pod red-pod
-
-echo '========================='
-
-echo 'kubenetes Pod blue-pod 실행'
-echo 'kubectl apply -f blue.yaml'
-kubectl apply -f blue.yaml
-
-echo '========================='
-
-echo 'kubenetes Pod green-pod 실행'
-echo 'kubectl apply -f green.yaml'
-kubectl apply -f green.yaml
-
-echo '========================='
 
 echo 'kubenetes Pod red-pod 실행'
 echo 'kubectl apply -f red.yaml'
