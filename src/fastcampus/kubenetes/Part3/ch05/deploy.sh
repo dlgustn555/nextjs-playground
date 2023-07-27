eba580b772e79e3186342e3cf9a2c9aec9e8d1d2
hyunsu-lee <hyunsu.lee@navercorp.com>
2023-07-27 12:33:20 +0900

    feat:ch05 실습-sky pod 테스트(#14)

--- src/fastcampus/kubenetes/Part3/ch05/deploy.sh
+++ src/fastcampus/kubenetes/Part3/ch05/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-echo 'hello POD 배포'
+echo '================ hello POD 배포 ================'
 cd hello
 
 docker build -t dlgustn555/hello .
@@ -10,7 +10,7 @@ kubectl delete pod hello-pod
 kubectl apply -f hello.yaml
 
 
-echo 'rose POD 배포'
+echo '================ rose POD 배포 ================'
 cd ../rose
 
 docker build -t dlgustn555/rose .
@@ -18,3 +18,12 @@ docker push dlgustn555/rose
 
 kubectl delete pod/rose-pod
 kubectl apply -f rose.yaml
+
+echo '================ sky POD 배포 ================'
+cd ../sky
+
+docker build -t dlgustn555/sky .
+docker push dlgustn555/sky
+
+kubectl delete pod/sky
+kubectl apply -f sky.yaml
