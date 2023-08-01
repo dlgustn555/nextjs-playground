6a862cbf99c1c3e72c6f1ecc6e3ffa0c31428683
hyunsu-lee <hyunsu.lee@navercorp.com>
2023-08-02 00:24:15 +0900

    마지막 미션 (#14)

---
+++ src/fastcampus/kubenetes/Part3/ch05/app/delivery/index.js
@@ -0,0 +1,20 @@
+const koa = require("koa");
+const Router = require("@koa/router");
+
+const app = new koa();
+const router = new Router();
+
+router.get("/status", async (ctx) => {
+  ctx.body = {
+    type: "Delivery [GET]/status",
+    message: "현재 배달 진행 상황입니다....",
+  };
+});
+
+app.use(router.routes()).use(router.allowedMethods());
+
+const PORT = 8080;
+
+app.listen(PORT, () => {
+  console.log(`Delivery Server is Running ON ${PORT}......!!!`);
+});
