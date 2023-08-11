64c6547e6ad79c37505d3ec1192dcefb9c2f76af
hyunsu-lee <hyunsu.lee@navercorp.com>
2023-08-11 19:09:15 +0900

    feat:실시간 채팅 (#33)

--- src/app/websocket-chat/page.tsx
+++ src/app/websocket-chat/page.tsx
@@ -1,7 +1,27 @@
+import WebSocketChart from "@/components/websocket_chat";
 import React from "react";
 
-const Page = () => {
-  return <div></div>;
+const WebSocketChartPage = () => {
+  return (
+    <div className="w-[900px] m-auto">
+      <h1 className="bg-gradient-to-r from-purple-100 to-gray-200 p-16 text-4xl font-bold">
+        웹소켓을 이용한 실시간 채팅
+      </h1>
+
+      <div className="p-16 space-y-8">
+        <div className="bg-gray-100 p-2">채팅목록</div>
+        <form className="space-x-4 flex">
+          <input
+            className="border-gray-400 p-4 rounded flex-1"
+            placeholder="채팅을 입력해 보세요."
+          />
+          <button className="bg-blue-600 text-white p-2 rounded">보내기</button>
+        </form>
+      </div>
+
+      <WebSocketChart />
+    </div>
+  );
 };
 
-export default Page;
+export default WebSocketChartPage;
