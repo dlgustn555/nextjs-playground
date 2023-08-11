7abc29a5bbaf784feaff73821dd826ad4fae060b
hyunsu-lee <hyunsu.lee@navercorp.com>
2023-08-11 19:18:26 +0900

    feat:실시간 채팅 (#33)

--- src/app/websocket-chat/page.tsx
+++ src/app/websocket-chat/page.tsx
@@ -5,7 +5,7 @@ const WebSocketChartPage = () => {
   return (
     <div className="w-[900px] m-auto">
       <h1 className="bg-gradient-to-r from-purple-100 to-gray-200 p-16 text-4xl font-bold">
-        웹소켓을 이용한 실시간 채팅
+        웹소켓을 이용한 실시간 채팅 : 0.0.5
       </h1>
 
       <div className="p-16 space-y-8">
