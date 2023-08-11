76b3909179dd69403cb1fb26c6c030042d8dbb93
hyunsu-lee <hyunsu.lee@navercorp.com>
2023-08-12 00:58:40 +0900

    feat: 실시간 챗팅 (#33)

--- src/components/websocket_chat/index.tsx
+++ src/components/websocket_chat/index.tsx
@@ -1,29 +1,68 @@
-"use client";
+"use client"
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react"
 
-const WebSocketChart = () => {
-  const [messages, setMessages] = useState<string[]>([]);
+const WebSocketChat: React.FC = () => {
+  const [socket, setSocket] = useState<WebSocket | null>(null)
+  const [message, setMessage] = useState("")
+  const [messages, setMessages] = useState<string[]>([])
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setMessage(e.target.value)
+  }
+
+  const handleButtonClick = (event: any) => {
+    socket?.send(message)
+    setMessage("")
+  }
+
   useEffect(() => {
-    const socket = new WebSocket(`ws://localhost:4000/ws`);
+    setSocket(new WebSocket(`ws://localhost:4000/ws`))
+  }, [])
 
-    socket.addEventListener("open", () => {
-      socket.send("Hello~!! I Connect!!");
-    });
+  useEffect(() => {
+    socket?.addEventListener("open", () => {
+      socket.send("Hello~!! I Connect!!")
+    })
 
-    socket.addEventListener("message", (event) => {
-      setMessages((prev) => [...prev, event.data]);
-      socket.close();
-    });
-  }, []);
+    socket?.addEventListener("message", (event) => {
+      if (typeof event.data === "string") {
+        setMessages((prev) => [...prev, event.data as string])
+      }
+    })
+
+    return () => {
+      socket?.close()
+    }
+  }, [socket])
 
   return (
-    <ul>
-      {messages.map((message, index) => (
-        <li key={index}>{message}</li>
-      ))}
-    </ul>
-  );
-};
+    <div className="p-16 space-y-8">
+      <div className="bg-gray-100 p-2">채팅목록</div>
+      <div className="space-x-4 flex">
+        <input
+          className="border-gray-400 p-4 rounded flex-1"
+          placeholder="채팅을 입력해 보세요."
+          value={message}
+          onChange={handleInputChange}
+        />
+        <button
+          className="bg-blue-600 text-white p-2 rounded"
+          onClick={handleButtonClick}
+        >
+          보내기
+        </button>
+      </div>
 
-export default WebSocketChart;
+      <ul>
+        {messages.map((message, index) => (
+          <li key={index} className="py-2">
+            {message}
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default WebSocketChat
